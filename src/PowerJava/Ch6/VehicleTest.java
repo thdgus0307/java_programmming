b0009cd01c3b807f1cbcab8ac55928a4a4f45693
thdgus0307 <bsh0374@gmail.com>
2025-08-28 23:39:02 +0900

    ch6

---
+++ src/PowerJava/Ch6/VehicleTest.java
@@ -0,0 +1,11 @@
+package PowerJava.Ch6;
+
+class Vehicle{
+
+}
+class Car extends Vehicle{}
+class Truck extends Vehicle{}
+class Bus extends Vehicle{}
+class SportsCar extends  Car{}
+public class VehicleTest {
+}
